05f0f4e97b6b6538c637f022c54b90535006c437
Mohamed Cherif <mocherifh0@gmail.com>
2021-11-09 21:26:23 +0100

    first version of the page

---
+++ static/styles/home.js
@@ -0,0 +1,11 @@
+$(document).ready (()=>{
+   $("#Menu").click(()=>{
+       $(".menu-Content").show(400);
+   })
+
+   $("#close").click(()=>{
+       $(".menu-Content").hide(200);
+       
+   })
+
+})
